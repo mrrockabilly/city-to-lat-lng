ce6496f9289f3b954e07226d9b9925731645aed5
Jett <Jett@Jett.lan>
2019-02-27 14:14:09 -0800

    added get geocode route

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
-
 const csvFilePath='uscitiesv1.4.csv'
 const csv=require('csvtojson')
+
 csv()
 .fromFile(csvFilePath)
-.then((jsonObj)=>{
-    app.get('/', (req, res) => res.send(jsonObj))
+.then((cityData)=>{
+    app.get('/cityGeoCode', (req, res) => {
+        let city = req.query.city;
+        let state = req.query.state;
+        console.log(req.query)
+        var found = cityData.find(function(element) {
+            return element.city === city && element.state_id === state;
+          });
+        if(found){
+            res.send(found)
+        } else {
+            res.send("Data not found.")
+        }
+        
+    })
 })
-
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
