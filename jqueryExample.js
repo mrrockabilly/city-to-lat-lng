5e8534b63edbd21ee8169fb0c957970a2799637c
Jett <Jett@Jett.lan>
2019-03-12 16:56:16 -0700

    remove jquery example

--- jqueryExample.js
+++
@@ -1,9 +0,0 @@
-var request = require('ajax-request');
-request('url', function(err, res, body) {});
- 
-request({
-  url: 'https://mrrockabilly.herokuapp.com/cityGeoCode/?city=Los+Angeles&state=CA',
-  method: 'GET'
-}, function(err, res, body) {
-  console.log(body)
-});
